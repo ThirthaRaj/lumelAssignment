1934b2aee7e6f9d3d05ad3731c287e3cf530f439
ThirthaRaj <thirtharaj18@gmail.com>
2024-07-14 16:12:37 +0530

    Delete src/file-upload/file-upload.controller.d.ts

--- src/file-upload/file-upload.controller.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="multer" />
-import { FileUploadService } from './file-upload.service';
-export declare class FileUploadController {
-    private readonly fileUploadService;
-    constructor(fileUploadService: FileUploadService);
-    uploadFile(file: Express.Multer.File): Promise<void>;
-}
