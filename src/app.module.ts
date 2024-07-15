80fae423d52870b7fb83b6d0b4e9aaaac2e6bd8d
Thirtha008 <thirtharaj@snappymob.com>
2024-07-15 17:39:30 +0530

    Update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerAnalysisModule } from './customer-analysis/customer-analysis.module';
 import { dataSourceOptions } from './database/config';
 import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,}),TypeOrmModule.forRoot(dataSourceOptions), FileUploadModule],
+  imports: [ConfigModule.forRoot({isGlobal: true,}),TypeOrmModule.forRoot(dataSourceOptions), FileUploadModule, CustomerAnalysisModule],
   controllers: [AppController],
   providers: [AppService],
 })
